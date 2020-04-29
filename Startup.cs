e00557d653613f4ca883cf066775bede789db7bb
Wei Jing <jaychou123103@hotmail.com>
2020-04-30 03:03:17 +1200

    添加项目文件。

---
+++ Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(RentalMovieApp.Startup))]
+namespace RentalMovieApp
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
