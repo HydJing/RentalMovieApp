44c5741c28eca7fc2ec1a0397d157732a6333b62
Wei Jing <jaychou123103@hotmail.com>
2020-05-01 01:26:28 +1200

    Add first MVC files

---
+++ Controllers/MovieController.cs
@@ -0,0 +1,20 @@
+﻿using RentalMovieApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace RentalMovieApp.Controllers
+{
+    public class MovieController : Controller
+    {
+        // GET: Movie/Radnom
+        public ActionResult Random()
+        {
+            var movie = new Movie() { Name = "The Avengers" };
+
+            return View(movie);
+        }
+    }
+}
