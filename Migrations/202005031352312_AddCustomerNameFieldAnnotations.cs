2c2a9f7b8aac0421d1d1b7c533dbef428be955dc
Wei Jing <jaychou123103@hotmail.com>
2020-05-04 02:02:11 +1200

    Add membership types and migrations

---
+++ Migrations/202005031352312_AddCustomerNameFieldAnnotations.cs
@@ -0,0 +1,18 @@
+namespace RentalMovieApp.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class AddCustomerNameFieldAnnotations : DbMigration
+    {
+        public override void Up()
+        {
+            AlterColumn("dbo.Customers", "Name", c => c.String(nullable: false, maxLength: 255));
+        }
+        
+        public override void Down()
+        {
+            AlterColumn("dbo.Customers", "Name", c => c.String());
+        }
+    }
+}
---
+++ Models/MembershipType.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace RentalMovieApp.Models
+{
+    public class MembershipType
+    {
+        public int Id { get; set; } 
+        public short SignupFee { get; set; } 
+        public byte DurationInMonths { get; set; } 
+        public byte DiscountRate { get; set; }
+
+    }
+}
