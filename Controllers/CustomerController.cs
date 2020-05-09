d1a7ad5f3cc984e1806624838275a66d1349f7c5
Wei Jing <jaychou123103@hotmail.com>
2020-05-10 02:30:16 +1200

    Add customer form validation

--- Controllers/CustomerController.cs
+++ Controllers/CustomerController.cs
@@ -25,6 +25,7 @@ namespace RentalMovieApp.Controllers
         {
             var ViewModel = new CustomerFormViewModel
             {
+                Customer = new Customer(),
                 MembershipTypes = _context.MembershipTypes.ToList()
             };
             return View("CustomerForm", ViewModel);
@@ -52,6 +53,18 @@ namespace RentalMovieApp.Controllers
         [HttpPost]
         public ActionResult Save(Customer customer)
         {
+            if (!ModelState.IsValid)
+            {
+                var viewModel = new CustomerFormViewModel
+                {
+                    Customer = customer,
+                    MembershipTypes = _context.MembershipTypes.ToList()
+                };
+
+                return View("CustomerForm", viewModel);
+            }
+
+
             if (customer.Id == 0)
             {
                 _context.Customers.Add(customer);
